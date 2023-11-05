7a5ec772c643633ccdf2d2c2331e20680b137dab
KodeJaROM <138987362+KodeJaROM@users.noreply.github.com>
2023-11-04 19:47:12 -0500

    Initial commit to create repo

---
+++ Parking Lot/task/src/parking/Main.kt
@@ -0,0 +1,7 @@
+package parking
+
+fun main() {
+    println("${readln()} car has parked.")
+    println("${readln()} car left the parking lot.")
+    println("${readln()} car just parked here.")
+}
