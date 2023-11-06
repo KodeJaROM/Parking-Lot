94fdd26e1ae8aee82eaf59482b37ab13d92a42a4
KodeJaROM <138987362+KodeJaROM@users.noreply.github.com>
2023-11-06 07:11:25 -0600

    Added statistics to pass stage 4

--- Parking Lot/task/src/parking/Main.kt
+++ Parking Lot/task/src/parking/Main.kt
@@ -14,8 +14,9 @@ fun main() {
             } else {
                 val firstAvailableSpot = parkingLot.indexOf("0")
                 if (firstAvailableSpot != -1) {
+                    val carInfo = input[1] + " " + input[2]
                     parkingLot[firstAvailableSpot] =
-                        input[1] // This stores the license plate number of the car
+                        carInfo // This stores the license plate number and color of the car
                     println("${input[2]} car parked in spot ${firstAvailableSpot + 1}.")
                 } else {
                     println("Sorry, the parking lot is full.")
@@ -40,15 +41,38 @@ fun main() {
 
         //        function to specify the size of the parking lot
         fun createParkingLot() {
-                parkingLot = MutableList(input[1].toInt()) { "0" }
-                println("Created a parking lot with ${input[1]} spots.")
+            parkingLot = MutableList(input[1].toInt()) { "0" }
+            println("Created a parking lot with ${input[1]} spots.")
         }
+
+        //        function to print the statistics of the parking lot
+        fun showStatus() {
+            if (parkingLot.isEmpty()) {
+                println("Sorry, a parking lot has not been created.")
+            } else {
+                var isEmpty = true
+
+                for (index in parkingLot.indices) {
+                    val spot = parkingLot[index]
+                    if (spot != "0") {
+                        println("${index + 1} $spot")
+                        isEmpty = false
+                    }
+                }
+                if (isEmpty) {
+                    println("Parking lot is empty.")
+                }
+            }
+        }
+
+
 
         when (input[0]) {
             "exit" -> break
             "park" -> park()
             "leave" -> leave()
             "create" -> createParkingLot()
+            "status" -> showStatus()
         }
     }
 }
